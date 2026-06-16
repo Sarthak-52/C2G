990d25ac3940bd1495f2a060ff71e3457df21566
Sarthak Sharma <sharmasarthak934@gmail.com>
2026-06-16 15:01:34 +0530

    [C2G] Submit: Coding Assignment - Question 1

---
+++ assignments/Coding Assignment/Question 1/question.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        int n = sc.nextInt();
+
+        if(n< 20){
+            System.out.println("Charge Soon");
+        }else{
+            System.out.println("Battery OK");
+        }
+        
+    }
+}
