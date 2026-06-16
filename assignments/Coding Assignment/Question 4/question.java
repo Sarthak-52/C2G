f65179649b23b02e7ea2ed0020a51fe1b33222c5
Sarthak Sharma <sharmasarthak934@gmail.com>
2026-06-16 15:06:44 +0530

    [C2G] Submit: Coding Assignment - Question 4

---
+++ assignments/Coding Assignment/Question 4/question.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        int speed = sc.nextInt();
+
+        if(speed < 10){
+            System.out.println("Slow");
+        }else if(10 < speed < 50){
+            System.out.println("Average");
+        }else{
+            System.out.println("Fast");
+        }
+        
+    }
+}
