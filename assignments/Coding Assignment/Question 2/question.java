4270bd40ef4347212c11876fee1b8e35e153a394
Sarthak Sharma <sharmasarthak934@gmail.com>
2026-06-16 15:03:13 +0530

    [C2G] Submit: Coding Assignment - Question 2

---
+++ assignments/Coding Assignment/Question 2/question.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        int day = sc.nextInt();
+
+        if(day ==1){
+            System.out.println("Discount available");
+        }else{
+            System.out.println("no Discount");
+        }
+        
+    }
+}
