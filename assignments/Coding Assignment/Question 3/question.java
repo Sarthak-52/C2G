89547cb082c6ddeb7318b3d57c8c467b216667fc
Sarthak Sharma <sharmasarthak934@gmail.com>
2026-06-16 15:04:46 +0530

    [C2G] Submit: Coding Assignment - Question 3

---
+++ assignments/Coding Assignment/Question 3/question.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        int temp = sc.nextInt();
+
+        if(temp >= 30){
+            System.out.println("Fan ON");
+        } else{
+            System.out.println("Fan OFF");
+        }
+        
+    }
+}
